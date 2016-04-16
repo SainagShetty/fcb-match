46bb93d37c04145a8d8ddd60f106594ff5ffb1e7
sainag-shetty <shettysainag@gmail.com>
2016-04-17 01:15:23 +0530

    Parse and displaying

--- app.py
+++ app.py
@@ -5,11 +5,21 @@ import requests
 import datetime
 from bs4 import BeautifulSoup as bs
 
+url = 'http://www.fcbarcelona.com/football'
+response = requests.get(url)
+html = response.content
+soup = bs(html)
+opp = soup.find('span', {'class': 'away'}).contents
+ven = soup.find('span', {'class': 'venue'}).contents
+dat = soup.find('span', {'class': 'date-time'}).contents
+ti = soup.find('span', {'class': 'date-time local'}).contents
+print("F C Barcelona is playing "+opp[0]+" at "+ven[0])
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('index.html';
+    return render_template('index.html',opp = opp[0], ven = ven[0], days = days, hours = hours, minutes = minutes);
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
